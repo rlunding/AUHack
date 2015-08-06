c257acec332c1a8224fe2980d4f3d92a5208ea3a
Lunding <lunding@me.com>
2015-08-06 11:04:57 +0200

    Register page updated.

--- js/main2.js
+++ js/main2.js
@@ -3,6 +3,43 @@
  */
 
 
-$( document ).ready(function() {
+var smallWindow;
 
+$( document ).ready(function() {
+    function checkWidth() {
+        var windowSize = $(window).width();
+        if (windowSize < 768) {
+            smallWindow = true;
+            $(".faq-section p").slideUp(0);
+        } else {
+            $(".faq-section p").slideDown(0);
+            smallWindow = false;
+        }
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
+
+    /*$(".expand-icon").click(function(){
+        var element = $(this).parent().next();
+        if (element.is(":visible")){
+            element.slideUp(500);
+        } else {
+            $(".expand-icon-selected").slideUp(500).removeClass("expand-icon-selected");
+            element.slideDown(500);
+            element.addClass("expand-icon-selected");
+        }
+    });*/
+    $(".faq-section h4").click(function(){
+        if (smallWindow) {
+            var element = $(this).next();
+            if (element.is(":visible")){
+                element.slideUp(500);
+            } else {
+                $(".faq-item-selected").slideUp(500).removeClass("faq-item-selected");
+                element.slideDown(500);
+                element.addClass("faq-item-selected");
+            }
+        }
+
+    });
 });
